2c69c3f480cf22a1a53df3df0cd7d85a15cc48cc
insertvalue <insertvalue@naver.com>
2023-04-08 13:01:43 +0900

    mapper 코드 추가 화면 구성 1

---
+++ src/main/java/com/tenco/bank/controller/TestController1.java
@@ -0,0 +1,23 @@
+package com.tenco.bank.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class TestController1 {
+	
+	@GetMapping("/main")
+	public String mainTest() {
+		//   yml 파일 설정
+		//   prefix: /WEB-INF/view/
+	    //   suffix: .jsp 
+		
+		// ViewResolver 동작 
+		// /WEB-INF/view/layout/main.jsp
+		System.out.println("코드 실행 확인");
+		return "/layout/main";
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/view/layout/main.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+
+<%@ include file="/WEB-INF/view/layout/header.jsp" %>
+			
+		<!-- 변경 될 영역으로 처리 할 예정  -->
+		<div class="col-sm-8">
+			<h2>TITLE HEADING</h2>
+			<h5>...</h5>
+		</div>
+		<!-- 변경 될 영역 end   -->
+			
+				
+<%@ include file="/WEB-INF/view/layout/footer.jsp" %>
